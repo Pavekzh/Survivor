ea428c8c89d5c9efe5f944221aadabb34bb21faa
Denis Zhukovin <pavekzh@gmail.com>
2023-07-06 14:23:14 +0300

    Created level environment, added simple camera follow

---
+++ Assets/Scripts/Services/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+public class CameraFollow:MonoBehaviour
+{
+    [SerializeField] Transform target;
+    [SerializeField] float smoothFactor = 2;
+
+    Vector3 offset;
+
+    private void Awake()
+    {
+        offset = transform.position - target.position;
+    }
+
+    private void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, target.position + offset, smoothFactor * Time.deltaTime);
+    }
+}
+
