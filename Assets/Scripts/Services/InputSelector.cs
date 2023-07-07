f3e93cd097b7dafc8de309d519b28a9656f8d8bc
Denis Zhukovin <pavekzh@gmail.com>
2023-07-07 10:51:23 +0300

    Imported joystick, added joystick input & input detector selector

---
+++ Assets/Scripts/Services/InputSelector.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+
+public class InputSelector:InputDetector
+{
+    [SerializeField] private AxesInputDetector axesInputDetector;
+    [SerializeField] private JoysticksInputDetector joysticksInputDetector;
+
+    private InputDetector selectedDetector;
+
+    public override Vector2 GetAttackInput()
+    {
+        return selectedDetector.GetAttackInput();
+    }
+
+    public override Vector2 GetMoveInput()
+    {
+        return selectedDetector.GetMoveInput();
+    }
+
+    private void Start()
+    {
+#if UNITY_ANDROID || UNITY_IOS
+        selectedDetector = joysticksInputDetector;
+        joysticksInputDetector.Enable();
+#elif UNITY_STANDALONE_WIN || UNITY_EDITOR
+        selectedDetector = axesInputDetector;
+        joysticksInputDetector.Disable();
+#endif
+    }
+
+
+}
+
---
+++ Assets/Scripts/Services/JoysticksInputDetector.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+
+public class JoysticksInputDetector : InputDetector
+{
+    [SerializeField] FixedJoystick moveJoystick;
+    [SerializeField] FixedJoystick attackJoystick;
+
+    public void Disable()
+    {
+        moveJoystick.gameObject.SetActive(false);
+        attackJoystick.gameObject.SetActive(false);
+    }
+
+    public void Enable()
+    {
+        moveJoystick.gameObject.SetActive(true);
+        attackJoystick.gameObject.SetActive(true);
+    }
+
+    public override Vector2 GetAttackInput()
+    {
+        Vector2 input = attackJoystick.Direction;
+        return input.normalized;
+    }
+
+    public override Vector2 GetMoveInput()
+    {
+        Vector2 input = moveJoystick.Direction;
+        return input;
+    }
+}
+
