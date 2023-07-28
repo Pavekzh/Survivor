3752be4d1b74d34bb0ebb4e78b3cff1baf3ea8bb
Denis Zhukovin <pavekzh@gmail.com>
2023-07-28 17:37:33 +0300

    Refactored selecting players gun

--- Assets/Scripts/Services/GunSelector.cs
+++ Assets/Scripts/Services/GunSelector.cs
@@ -1,29 +1,13 @@
 ﻿using UnityEngine;
 using Fusion;
+using System.Collections.Generic;
 
 public class GunSelector : NetworkBehaviour
 {
     [SerializeField] private GunFactory[] guns;
 
-    private System.Collections.Generic.List<int> freeGuns;
     [Networked] private int firstPlayerSelected { get; set; } = -1;
     [Networked] private int secondPlayerSelected { get; set; } = -1;
-
-    private System.Collections.Generic.List<int> FreeGuns
-    {
-        get
-        {
-            if(freeGuns == null)
-            {
-                freeGuns = new System.Collections.Generic.List<int>();
-                for(int i = 0; i< guns.Length; i++)
-                {
-                    freeGuns.Add(i);
-                }
-            }
-            return freeGuns;
-        }
-    }
 
     private Character character;
     private GameBootstrap gameBootstrap;
@@ -38,8 +22,12 @@ public class GunSelector : NetworkBehaviour
     {
         if(HasStateAuthority)
         {
-            firstPlayerSelected = SelectRandomGun();
-            secondPlayerSelected = SelectRandomGun();
+            firstPlayerSelected = Random.Range(0, guns.Length);
+            do
+            {
+                secondPlayerSelected = Random.Range(0, guns.Length);
+            } while (secondPlayerSelected == firstPlayerSelected);
+
         }
         InstantiateLocalPlayerGun(character);
     }
@@ -58,21 +46,6 @@ public class GunSelector : NetworkBehaviour
             InstantiatePlayerGun(character, guns[firstPlayerSelected],false);
         else
             InstantiatePlayerGun(character, guns[secondPlayerSelected],false);
-    }
-
-    private int SelectRandomGun()
-    {
-        if(FreeGuns.Count != 0)
-        {
-            int random = Random.Range(0, FreeGuns.Count);
-            int selected = freeGuns[random];
-            FreeGuns.RemoveAt(random);
-            return selected;
-        }
-        else
-        {
-            throw new System.IndexOutOfRangeException("Players count greater than gun variants");
-        }
     }
 
     private void InstantiatePlayerGun(Character character,GunFactory gunFactory,bool isRemotePlayer)
